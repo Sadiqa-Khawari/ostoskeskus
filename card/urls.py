0438a4d41f6f6e471be115f4e6a91c5eea1fd493
Sadiqa <53186@edu.raseko.fi>
2025-08-12 12:34:46 +0300

    Lissättu

---
+++ card/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.card_list, name='card_list'),
+    path('add/', views.add_card, name='add_card'),
+]
--- card/views.py
+++ card/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Card
+from .forms import CardForm
 
-# Create your views here.
+def card_list(request):
+    cards = Card.objects.all()
+    return render(request, 'card/card_list.html', {'cards': cards})
+
+
+def add_card(request):
+    if request.method == 'POST':
+        form = CardForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('card_list')
+    else:
+        form = CardForm()
+    return render(request, 'card/add_card.html', {'form': form})
